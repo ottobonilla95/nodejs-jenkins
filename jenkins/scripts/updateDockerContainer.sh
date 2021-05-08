a2200b7a109d89e15270fa0cb79a5a7e41a11fbb
ottobonilla95 <mr.bonilla51@gmail.com>
2021-05-08 18:26:48 +0100

    test

--- jenkins/scripts/updateDockerContainer.sh
+++ jenkins/scripts/updateDockerContainer.sh
@@ -2,16 +2,17 @@
 IMAGE_NAME="ottobonilla95/nodejsjenkins"
 
 # stop container
+echo "stoping container"
 docker stop $(docker ps -q --filter ancestor=$IMAGE_NAME )
 
-# delete container
-docker rm $(docker ps -q --filter ancestor=$IMAGE_NAME )
+# # delete container
+# docker rm $(docker ps -q --filter ancestor=$IMAGE_NAME )
 
-# delete image
-docker rmi $(docker images  --filter reference=$IMAGE_NAME  --format "{{.ID}}" )
+# # delete image
+# docker rmi $(docker images  --filter reference=$IMAGE_NAME  --format "{{.ID}}" )
 
-# get lastest image
-docker pull $IMAGE_NAME
+# # get lastest image
+# docker pull $IMAGE_NAME
 
-# run image
-docker run -p 3005:3005 $IMAGE_NAME
+# # run image
+# docker run -p 3005:3005 $IMAGE_NAME
