98d2f5ce6d43b27eca2137903ea5ded81360cd50
ottobonilla95 <mr.bonilla51@gmail.com>
2021-05-14 11:08:50 +0100

    aja

--- jenkins/scripts/updateDockerContainer.sh
+++ jenkins/scripts/updateDockerContainer.sh
@@ -1,71 +1,65 @@
-# set -e
+set -e
 
-# # image name
-# IMAGE_NAME="ottobonilla95/nodejsjenkins"
+# image name
+IMAGE_NAME="bartrcash/webapi"
 
-# # stop container
-# echo "Stoping container..."
+# stop container
+echo "Stoping container..."
 
-# CONTAINER_ID=$(docker ps -q --filter ancestor=$IMAGE_NAME )
+CONTAINER_ID=$(sudo docker ps -q --filter ancestor=$IMAGE_NAME )
 
-# if [ $CONTAINER_ID ]
-# then
-#     set -x
-#     docker stop $CONTAINER_ID
-#     set +x
+if [ $CONTAINER_ID ]
+then
+    set -x
+    sudo docker stop $CONTAINER_ID
+    set +x
 
-#     echo "Containter stopped."
-# else
-#     echo "Containter not found."
-# fi
-
-
-# # delete container
-# echo "Deleting container...."
-
-# CONTAINER_ID=$(docker ps --all -q --filter ancestor=$IMAGE_NAME )  
-
-# if [ $CONTAINER_ID ]
-# then
-#     set -x
-#     docker rm $CONTAINER_ID
-#     set +x
-#     echo "Containter deleted."
-# else
-#     echo "Containter not found."
-# fi
-
-# # delete image
-# echo "Deleting image...."
-# IMAGE_ID=$(docker images  --filter reference=$IMAGE_NAME  --format "{{.ID}}" )
-
-# if [ $IMAGE_ID ]
-# then
-#     set -x
-#     docker rmi $IMAGE_ID
-#     set +x
-#     echo "Image deleted."
-# else
-#     echo "Image not found."
-# fi
-
-# # get lastest image
-# echo "Getting the last version...."
-# set -x
-# docker pull $IMAGE_NAME
-# set +x
-
-# # run image
-# echo "Running image...."
-# set -x
-# docker run -p 3005:3000 -d $IMAGE_NAME
-# set +x
-
-# # End
-# echo "Docker tasks completed :)"
+    echo "Containter stopped."
+else
+    echo "Containter not found."
+fi
 
 
-echo "Holaaa..."
-cd /var
-sudo pwd
-sudo ls
+# delete container
+echo "Deleting container...."
+
+CONTAINER_ID=$(sudo docker ps --all -q --filter ancestor=$IMAGE_NAME )  
+
+if [ $CONTAINER_ID ]
+then
+    set -x
+    sudo docker rm $CONTAINER_ID
+    set +x
+    echo "Containter deleted."
+else
+    echo "Containter not found."
+fi
+
+# delete image
+echo "Deleting image...."
+IMAGE_ID=$(sudo docker images  --filter reference=$IMAGE_NAME  --format "{{.ID}}" )
+
+if [ $IMAGE_ID ]
+then
+    set -x
+    sudo docker rmi $IMAGE_ID
+    set +x
+    echo "Image deleted."
+else
+    echo "Image not found."
+fi
+
+# get lastest image
+echo "Getting the last version...."
+set -x
+sudo docker pull $IMAGE_NAME
+set +x
+
+# run image
+echo "Running image...."
+set -x
+sudo docker run -p 5000:80 -d $IMAGE_NAME
+set +x
+
+# End
+echo "Docker tasks completed :)"
